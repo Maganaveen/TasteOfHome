eda8626b80743987ef488fa1be806ef3ed35034e
Maganaveen <magalakshmi062002@gmail.com>
2025-07-02 10:12:04 +0000

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// 👇 VERY IMPORTANT: Set to the name of your GitHub repo
-export default defineConfig({
-  plugins: [react()],
-  base: '/TasteOfHome/'  // ✅ THIS FIXES THE 404 PATH ISSUE
-})
