5a9d629feb91cdb9fc1cfe20b6c77ba93380e15e
Maganaveen <magalakshmi062002@gmail.com>
2025-07-08 10:54:20 +0000

    rroute fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import HomePage from './components/HomePage';
