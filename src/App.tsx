e4a6232dfaf0d9cc00459aeddf67cd18fff2c260
Maganaveen <magalakshmi062002@gmail.com>
2025-07-09 04:08:35 +0000

    route fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import HomePage from './components/HomePage';
-import './App.css';
 import MenuPage from './components/MenuPage';
-import { CartProvider } from './components/CartContext';
 import OrderPage from './components/OrderPage';
-import { ToastContainer } from 'react-toastify';
 import TraditionalFoodPage from './components/TraditionalFoodPage';
 import ModernFoodPage from './components/ModernFoodPage';
-import VegFoodPage from './components/VegFoodPage'; // Import the new component
+import VegFoodPage from './components/VegFoodPage';
 import NonVegFoodPage from './components/NonVegFoodPage';
+import { CartProvider } from './components/CartContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
 function App() {
   return (
-    <Router basename="/TasteOfHome">
-
+    <Router>
       <CartProvider>
         <div className="App">
           <Routes>
@@ -28,7 +29,7 @@ function App() {
             <Route path="/foods/traditional" element={<TraditionalFoodPage />} />
             <Route path="/foods/modern" element={<ModernFoodPage />} />
             <Route path="/foods/veg" element={<VegFoodPage />} />
-          <Route path="/foods/nonveg" element={<NonVegFoodPage />} />
+            <Route path="/foods/nonveg" element={<NonVegFoodPage />} />
           </Routes>
           <ToastContainer position="top-right" autoClose={3000} />
         </div>
@@ -36,4 +37,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
